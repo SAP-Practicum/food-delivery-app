07dc06f48fb348052cf9d6667b831ec65d49220c
presl <preslavboriosov@abv.bg>
2025-04-30 01:00:03 +0300

    updated create order method to have a quantity

--- src/main/java/org/example/food_delivery_app/service/OrderService.java
+++ src/main/java/org/example/food_delivery_app/service/OrderService.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
